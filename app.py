8af62ac11ac79653acb50f64848ff82ffe2c8818
mr-ayush-yadav <mrshivamyadav888@gmail.com>
2026-02-26 09:29:41 +0000

    production fix

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ import os
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "sqlite:///database.db"
+)
 
 db = SQLAlchemy(app)
 
